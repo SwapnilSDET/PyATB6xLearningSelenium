cedb55e2374d867c34e413cdea5e2e634cd02a05
SwapnilSDET <sdetswapnil@gmail.com>
2025-12-27 21:42:01 +0530

    Pushing the task 2

---
+++ src/ex_03_Selenium_Mini_Projects_Locators/test_Selenium_12_Project_03_iDrive360.py
@@ -0,0 +1,39 @@
+"""
+// Locators - Find the Web elements
+// Open the URL https://www.idrive360.com/enterprise/login
+// Find the Email id** and enter the email as augtest_040823@idrive.com
+// Find the Pass inputbox** and enter 123456 .
+// Find and Click on the sign in button
+// Verify that the error message is shown "_**Your email, password, IP address or location did not match"**_
+"""
+
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+def test_iDrive360():
+    chrome_options = Options()
+    chrome_options.add_argument("--start-maximized")
+    driver = webdriver.Chrome(options=chrome_options)
+
+    driver.get("https://www.idrive360.com/enterprise/login")
+
+    time.sleep(5)
+
+    inputBox_email_id = driver.find_element(By.NAME, "username")
+    inputBox_email_id.send_keys("augtest_040823@idrive.com")
+
+    inputBox_password = driver.find_element(By.NAME, "password")
+    inputBox_password.send_keys("123456")
+
+    button_sign_in = driver.find_element(By.ID, "frm-btn")
+    button_sign_in.click()
+
+    time.sleep(20)
+
+    act_error_message = driver.find_element(By.CLASS_NAME, "id-card-title")
+
+    assert "Your free trial has expired!" == act_error_message.text
+
+    driver.quit()
